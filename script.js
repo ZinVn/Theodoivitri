226bcbbf0cbc0c63365b82888f8a66a15cf69b69
MinhNeko <145555970+ZinVn@users.noreply.github.com>
2025-03-25 17:15:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,13 @@
-document.getElementById('locationLink').addEventListener('click', function(event) {
-    event.preventDefault(); // Ngăn chặn hành động mặc định của liên kết
-
+function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition, showError);
+        navigator.geolocation.getCurrentPosition(showPosition);
     } else {
-        document.getElementById('locationResult').innerText = "Trình duyệt của bạn không hỗ trợ Geolocation.";
+        alert("Trình duyệt của bạn không hỗ trợ Geolocation.");
     }
-});
+}
 
 function showPosition(position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
-    document.getElementById('locationResult').innerText = `Vị trí của bạn: Latitude: ${latitude}, Longitude: ${longitude}`;
-}
-
-function showError(error) {
-    switch(error.code) {
-        case error.PERMISSION_DENIED:
-            document.getElementById('locationResult').innerText = "Người dùng từ chối yêu cầu xác định vị trí.";
-            break;
-        case error.POSITION_UNAVAILABLE:
-            document.getElementById('locationResult').innerText = "Thông tin vị trí không khả dụng.";
-            break;
-        case error.TIMEOUT:
-            document.getElementById('locationResult').innerText = "Yêu cầu xác định vị trí đã hết thời gian.";
-            break;
-        case error.UNKNOWN_ERROR:
-            document.getElementById('locationResult').innerText = "Đã xảy ra lỗi không xác định.";
-            break;
-    }
+    alert("Vị trí của bạn: " + latitude + ", " + longitude);
 }
